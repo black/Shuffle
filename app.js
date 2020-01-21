cf5cfb9586327438af38f5dd0f99538c4523a454
Λ B H I N Λ V <ixd.abhi@gmail.com>
2020-01-21 17:12:32 -0500

    Project Finished

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express'), 
+    walk = require('walk'),
+    app = express(),
+    ALLfiles = [];
+
+let server = app.listen(9090, () => {
+    console.log("Random Picture 9090");
+    getAllfiles();
+});
+
+app.use(express.static('pictures')); 
+
+app.get('/:imgtag', (req, res) => {
+    let typeImg = req.params.imgtag;
+    console.log(typeImg);
+    res.statusCode = 302;
+    if (typeImg == 'default') res.setHeader("Location", "default.png");
+    else if (typeImg == 'random') res.setHeader("Location", ALLfiles[getRandomInt(ALLfiles.length)]); 
+    res.end();
+});
+
+let getAllfiles = () => {
+    let walker = walk.walk('./pictures', { followLinks: false });
+    walker.on('file', (root, stat, next) => {
+        // Add this file to the list of files
+        ALLfiles.push(stat.name);
+        next();
+    });
+
+    walker.on('end', () => {
+        console.log("loaded all images...", ALLfiles.length);
+    });
+
+}
+
+let getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+}

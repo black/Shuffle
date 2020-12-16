6245deb0fc59b7a15dce5d93b49757b193b2cf5a
Λ B H I N Λ V <ixd.abhi@gmail.com>
2020-12-16 15:56:17 -0500

    making it desktop app

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-const express = require('express'), 
+const express = require('express'),
     walk = require('walk'),
     app = express(),
     ALLfiles = [];
 
-let server = app.listen(9091, () => {
+app.listen(9091, () => {
     console.log("Random Picture 9091");
     getAllfiles();
 });
 
-app.use(express.static('pictures')); 
+app.use(express.static('pictures'));
 
 app.get('/:imgtag', (req, res) => {
     let typeImg = req.params.imgtag;
     console.log(typeImg);
     res.statusCode = 302;
     if (typeImg == 'default') res.setHeader("Location", "default.png");
-    else if (typeImg == 'random') res.setHeader("Location", ALLfiles[getRandomInt(ALLfiles.length)]); 
+    else if (typeImg == 'random') res.setHeader("Location", ALLfiles[getRandomInt(ALLfiles.length)]);
     res.end();
 });
 
 let getAllfiles = () => {
-    let walker = walk.walk('./pictures', { followLinks: false });
+    let walker = walk.walk('./pictures', {
+        followLinks: false
+    });
     walker.on('file', (root, stat, next) => {
         // Add this file to the list of files
         ALLfiles.push(stat.name);
